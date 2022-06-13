848beffac0c86da83082a7899d0b92313ec54c67
ebruyardim <ebruyardim98@gmail.com>
2022-06-13 23:18:16 +0300

    question2 is solved

--- assessment/html-css-js/question2.js
+++ assessment/html-css-js/question2.js
@@ -1,21 +1,32 @@
 // ASSESSMENT:
 // Fill inside of the optimizeFunction function!
 
+let lastTask={"number":"", "result":"", "func": ""};
+
 function doExpensiveTask(input) {
   const result = 2 * input;
   console.log("Doing expensive task...:", result);
   return result;
 }
 
-function optimizeFunction(func) {
+function optimizeFunction(func, number) {
   // You shouldn't need to edit anywhere else
   // Do your work inside this function
   // SOLUTION:
 
+  if (lastTask.func===func & lastTask.number===number){
+      console.log("Same input, no need to calculate: ", lastTask.result);
+      return lastTask.result;
+  }
+  else{
+    result = func(number);
+    lastTask = {"number":number, "result":result, "func": func};
+  }
+
 }
 
 // anOptimizedFunc shouldn't execute the expensive task if new input is same as the previous one
-const anOptimizedFunc = optimizeFunction(doExpensiveTask);
+const anOptimizedFunc = (number) => optimizeFunction(doExpensiveTask, number);
 anOptimizedFunc(2); // Should print: Doing expensive task...: 4
 anOptimizedFunc(2); // Should print: Same input, no need to calculate: 4
 anOptimizedFunc(4); // Should print: Doing expensive task...: 8
@@ -30,7 +41,7 @@ function doAnotherExpensiveTask(input) {
 }
 
 // We can optimize another function, which should also behave similarly
-const anotherOptimizedFunc = optimizeFunction(doAnotherExpensiveTask);
+const anotherOptimizedFunc = (num) => optimizeFunction(doAnotherExpensiveTask, num);
 anotherOptimizedFunc(2); // Should print: Doing another expensive task...: 20
 anotherOptimizedFunc(2); // Should print: Same input, no need to calculate: 20
 anotherOptimizedFunc(4); // Should print: Doing another expensive task...: 40
