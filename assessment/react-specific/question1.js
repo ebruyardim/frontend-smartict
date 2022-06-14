b7101d61b7bfb97d53c05054eff92cdcb8f5943b
ebruyardim <ebruyardim98@gmail.com>
2022-06-14 18:09:44 +0300

    react question1 is solved

--- assessment/react-specific/question1.js
+++ assessment/react-specific/question1.js
@@ -12,9 +12,107 @@
 
 // SOLUTION:
 // React and ReactDOM is already imported in index.html
+
+const INITIAL_VALUE = {
+  loading: false,
+  error: false,
+  data: null
+};
+
+const ACTION_TYPES = {
+  START_FETCH: 0,
+  FETCH_SUCCESS: 1,
+  FETCH_FAILURE: 2,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTION_TYPES.START_FETCH:
+      return {
+        ...state,
+        loading: true
+      };
+    case ACTION_TYPES.FETCH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case ACTION_TYPES.FETCH_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
+
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, INITIAL_VALUE);
+  const [pageNumber, setpageNumber] = React.useState(0);
+
+  React.useEffect(() => {
+
+    dispatch({type: ACTION_TYPES.START_FETCH})
+
+    fetch(`https://jsonplaceholder.typicode.com/todos?_start=`+(pageNumber*10)+`&_limit=10`)
+      .then((response) => {
+        if(response.ok) {
+          return response.json()
+        }
+        throw new Error("HTTP RESPONSE ERROR")
+
+      })
+
+      .then((json) => dispatch({type: ACTION_TYPES.FETCH_SUCCESS, payload: json}))
+
+      .catch(err => {
+        console.log(err);
+        dispatch({type: ACTION_TYPES.FETCH_FAILURE})
+      })
+  }, [pageNumber]);
+
+
+
   return (
-    <p>Hello from question1!</p>
+    <div>
+      {state.loading && <p>Loading ....</p>}
+      {state.error && <p>ERROR HAPPENED!!</p>}
+      {state.data && 
+      <div>
+
+      <table>
+        <thead>
+          <tr>
+          <th>Id</th>
+          <th>User Id</th>
+          <th>Title</th>
+          <th>Completed</th>
+          </tr>
+        </thead>
+        <tbody>
+        {state.data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.id}</td>
+              <td>{val.userId}</td>
+              <td>{val.title}</td>
+              <td>{val.completed.toString()}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+      <br></br>
+      <button disabled={pageNumber===0} onClick={() => setpageNumber(pageNumber - 1)}>Previous</button>
+      <button onClick={() => setpageNumber(pageNumber + 1)}>Next</button>
+
+      </div>
+    }
+    </div>
   );
 }
 
